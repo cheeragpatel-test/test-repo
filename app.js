33b0e05ec54a5e6c2e0c943ad8b9cce859538b1a
Cheerag Patel <cheeragpatel@github.com>
2023-09-28 11:08:39 -0400

    Updated body

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   res.statusCode = 200;
   res.setHeader('Content-Type', 'text/plain');
-  res.end('<html><head><title>Testing Docker Builds</title></head><body>Hello, World!</body></html>');
+  res.end('Hello, World!\n');
 });
 
 server.listen(8080, '0.0.0.0', () => {
